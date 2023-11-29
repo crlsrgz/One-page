7ac0bade85ea0d4045c51dbd162ffd115809539f
crlsrgz <course@carlosfx.com>
2023-11-29 23:50:28 +0100

    test: Object info

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ComponentMainNav from "./components/navigation.component";
-import SceneHomepage from "./components/scene.component.homepage";
+// import SceneHomepage from "./components/scene.component.homepage";
 import ComponentDetail from "./components/component.detail";
 
 const App = () => {
@@ -10,8 +10,11 @@ const App = () => {
   //  document.querySelector("body").style["overflow"] = "hidden";
 
   return (
-    <main className="box-border flex flex-col">
+    <main className="box-border flex flex-col" style={{ zIndex: "-1" }}>
       <div className="canvas-element h-screen w-screen">
+        <div className="info hideMe absolute left-4 top-16 h-2/4 w-96 text-zinc-100">
+          hello
+        </div>
         <ComponentMainNav />
         {/* <SceneHomepage /> */}
         {/* <SceneLandscape /> */}
