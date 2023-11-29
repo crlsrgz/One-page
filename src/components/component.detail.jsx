85ee6a174d01d8f5b264ba557adb2c1103ca7bad
crlsrgz <course@carlosfx.com>
2023-11-29 13:14:11 +0100

    test: importing detail model parts

--- src/components/component.detail.jsx
+++ src/components/component.detail.jsx
@@ -1,6 +1,8 @@
 import {
+  Box,
   Edges,
   GizmoViewport,
+  Html,
   OrbitControls,
   Outlines,
   PivotControls,
@@ -10,16 +12,19 @@ import {
 } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { Perf } from "r3f-perf";
-import { Suspense } from "react";
-import { BoxGeometry } from "three";
+import { Suspense, useState } from "react";
+import { BoxGeometry, MeshNormalMaterial } from "three";
+import { ModelDetail } from "./model.detail";
 
 export default function ComponentDetail() {
-  const model = useGLTF("./wall.glb");
-  console.log(model.scene.children);
+  // const model = useGLTF("./wall.glb");
+  // console.log(model.scene.children);
+  // console.log(model.scene.animations);
+
   return (
     <Canvas camera={{ position: [-18, 1, 15], near: 0.01, far: 100, fov: 45 }}>
       <color args={["black"]} attach="background" />
-
+      <ambientLight intensity={2} />
       <Perf />
       <GizmoViewport
         axisColors={["red", "green", "blue"]}
@@ -27,35 +32,28 @@ export default function ComponentDetail() {
         position={[-2, 0, 0]}
         scale={0.4}
       />
+
       <OrbitControls makeDefault />
+
       <Stage
         contactShadow={{ opacity: 0.5, blur: 5 }}
         environment="city"
-        preset={"portrait"}
         intensity={0.8}
       >
-        {model.scene.children.map((item, index) => {
-          return (
-            <>
-              <PivotControls
-                anchor={[0, 0, 0]}
-                lineWidth={1.2}
-                scale={0.5}
-                disableRotations
-              >
-                <mesh
-                  key={index}
-                  geometry={item.geometry}
-                  material={item.material}
-                  position={item.position}
-                  rotation={item.rotation}
-                >
-                  {/* <Edges geometry={item.geometry} /> */}
-                </mesh>
-              </PivotControls>
-            </>
-          );
-        })}
+        {/* <primitive object={model.scene} /> */}
+
+        <>
+          <PivotControls
+            anchor={[0, 0, 0]}
+            lineWidth={1.2}
+            scale={0.5}
+            disableRotations
+            disableAxes
+            disableSliders
+          ></PivotControls>
+          <ModelDetail />
+          <Box position={[0, -2, 0]} />
+        </>
       </Stage>
     </Canvas>
   );
