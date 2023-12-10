016d2633cc1db59fc2db445df47359a082049250
crlsrgz <course@carlosfx.com>
2023-12-10 19:46:34 +0100

    Scene-detail::test: animating objets inside of app

--- src/components/scene-detail/model.detail.jsx
+++ src/components/scene-detail/model.detail.jsx
@@ -15,10 +15,39 @@ import {
 } from "@react-three/drei";
 import { MeshStandardMaterial } from "three";
 import { useControls } from "leva";
+import { useFrame } from "@react-three/fiber";
 
 function ModelParts(props) {
   const [hover, setHover] = useState(false);
   const [nameVisible, setNameVisible] = useState(false);
+  const [alternatePosition, setAlternatePosition] = useState(props.position);
+
+  const refModelPart = useRef();
+  const externPositon = {
+    x: props.position.x,
+    y: props.position.y + Math.random() * 0.75 + 1,
+    z: props.position.z,
+  };
+  useFrame(({ clock }) => {
+    // console.log(refCube.current.rotation);
+    setTimeout(() => {
+      if (props.name.toLowerCase().includes("ziegel")) {
+        refModelPart.current.position.lerp(externPositon, 0.05);
+      }
+      if (props.name.toLowerCase().includes("sparren")) {
+        refModelPart.current.position.lerp(
+          {
+            x: props.position.x - 1,
+            y: props.position.y + -1,
+            z: props.position.z,
+          },
+          0.008,
+        );
+      } else {
+        refModelPart.current.position.lerp(props.position, 0.2);
+      }
+    }, 1000);
+  });
 
   function displayName(e) {
     e.stopPropagation();
@@ -26,22 +55,33 @@ function ModelParts(props) {
       setNameVisible(false);
     } else {
       setNameVisible(true);
+      console.log(props.position);
+      console.log(props.name);
+      console.log(props.name.toLowerCase().includes("sparren"));
+
+      if (props.name.toLowerCase().includes("ziegel")) {
+        let { x, y, z } = props.position;
+        y += 1;
+        setAlternatePosition({ x, y, z });
+      }
+      if (props.name.toLowerCase().includes("sparren")) {
+        let { x, y, z } = props.position;
+        x += -1;
+        y += -1;
+        setAlternatePosition({ x, y, z });
+      }
     }
-    const div = document.querySelector(".info");
-    div.classList.remove("hideMe");
-    div.textContent = props.name;
   }
   function hideName(e) {
     e.stopPropagation();
     setNameVisible(false);
-
-    const div = document.querySelector(".info");
-    div.classList.add("hideMe");
+    setAlternatePosition(props.position);
   }
 
   return (
     <>
       <mesh
+        ref={refModelPart}
         name={props.name}
         castShadow
         receiveShadow
@@ -76,7 +116,6 @@ function ModelParts(props) {
 export function ModelDetail(props) {
   const model = useGLTF("/wall.glb");
   const animations = useAnimations(model.animations, model.scene);
-  console.log(animations.actions);
   /* ::::::::: Leva ::::::::: */
   // const { animationName } = useControls({
   //   animationName: {
@@ -101,38 +140,38 @@ export function ModelDetail(props) {
     }
   });
 
-  const [animationState, setAnimationState] = useState(true);
+  // const [animationState, setAnimationState] = useState(true);
 
-  useEffect(() => {
-    const action = animations.actions[props.action];
-    action.repetitions = 1;
-    action.clampWhenFinished = true;
-    action //
-      .reset() //
-      .fadeIn(0.5) //
-      .play();
+  // useEffect(() => {
+  //   const action = animations.actions[props.action];
+  //   action.repetitions = 1;
+  //   action.clampWhenFinished = true;
+  //   action //
+  //     .reset() //
+  //     .fadeIn(0.5) //
+  //     .play();
 
-    //Cleanup
-    return () => {
-      action.fadeOut(0.08);
-      console.log("dispose animation");
-    };
-    //////////////////////////
-    //////////////////////////
-    // const action = animations.actions[animationName];
-    // animations.actions["000"].repetitions = 2;
-    // animations.actions["000"].play();
-    // animations.mixer.addEventListener("finished", () => {
-    //   animations.actions["001"].play();
-  }, [props.action, animationState]);
+  //   //Cleanup
+  //   return () => {
+  //     action.fadeOut(0.08);
+  //     console.log("dispose animation");
+  //   };
+  //   //////////////////////////
+  //   //////////////////////////
+  //   // const action = animations.actions[animationName];
+  //   // animations.actions["000"].repetitions = 2;
+  //   // animations.actions["000"].play();
+  //   // animations.mixer.addEventListener("finished", () => {
+  //   //   animations.actions["001"].play();
+  // }, [props.action, animationState]);
 
   return (
     <>
-      <mesh>
+      {/* <mesh>
         <primitive object={model.scene} castShadow />
-      </mesh>
+      </mesh> */}
 
-      {/* {model.scene.children.map((element, index) => {
+      {model.scene.children.map((element, index) => {
         return (
           <ModelParts
             key={index}
@@ -143,7 +182,7 @@ export function ModelDetail(props) {
             position={element.position}
           />
         );
-      })} */}
+      })}
     </>
   );
 }
