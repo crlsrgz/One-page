adf965e6a0afaa4062680e68632a3a5ee823da1e
crlsrgz <course@carlosfx.com>
2024-03-21 14:04:58 +0100

    ::fix: navigation and tags display order

--- src/components/scene-product/component.InfoTags.jsx
+++ src/components/scene-product/component.InfoTags.jsx
@@ -11,7 +11,7 @@ export default function InfoTags({
 }) {
   return (
     <>
-      <Html center position={position}>
+      <Html center zIndexRange={10} position={position}>
         <button
           id={idString}
           className={`relative box-border h-12 w-12 cursor-pointer rounded-md border-2 border-slate-50 bg-slate-50 bg-opacity-10 font-urbanistMedium text-xl hover:animate-pulse-slow hover:bg-opacity-70 hover:text-zinc-900`}
--- src/components/scene-product/model.product.jsx
+++ src/components/scene-product/model.product.jsx
@@ -33,11 +33,10 @@ export function ModelProduct({ explode }) {
   const [isDrawerOpen] = useState(false);
   const actionsList = Object.keys(animations.actions);
   function playAnimation(
-    effectValue,
+    effectValue, // the following checks if state is false prevents first rerender
     actions,
     forwardAction,
     backwardAction,
-    // the following checks if state is false prevents first rerender
     state = false,
   ) {
     if (effectValue && !state) {
